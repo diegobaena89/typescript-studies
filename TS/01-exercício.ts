7142d48782dbeda501a421bdce63c50217621313
diegobaena89 <diegobf.cello@gmail.com>
2021-04-07 21:50:33 -0300

    add new studies on TS

---
+++ TS/01-exercício.ts
@@ -0,0 +1,54 @@
+// EXERCÍCIO 01
+/*
+const myBirthYear: number = 1989;
+const currentYear: number = 2021;
+
+console.log(myBirthYear, currentYear)
+
+const myAge: number = currentYear - myBirthYear;
+
+console.log(myAge)
+
+const ten: number = 10;
+console.log(`
+Em ${currentYear}, estarei com ${myAge + ten} anos`)
+*/
+
+// EXERCÍCIO 02
+const msg: string = "Grandes poderes trazem grandes responsabilidades"
+console.log(msg)
+
+const tom: string = 'Tom';
+const firstTom: string = 'Hanks'
+const secondTom: string = 'Cruise'
+
+const myFavoriteTom: string = `${tom} ${secondTom}`
+
+console.log(myFavoriteTom)
+
+const programmingLanguage: string = 'Javascript'
+const firstJ: string = programmingLanguage[0]
+const firstS: string = programmingLanguage[4]
+
+console.log(`
+${firstJ}${firstS}`)
+
+const totalLength = programmingLanguage.length
+console.log(totalLength)
+
+let randomWord: string = 'Araponga'
+const lastWord = randomWord[randomWord.length - 1]
+console.log(lastWord)
+
+const summerMessage: string = 'O VERÃO CHEGA NESTE FIM DE SEMANA'
+
+const msgToLowerCase = summerMessage.toLowerCase()
+console.log(msgToLowerCase)
+
+const winterMessage: string = 'o inverno está chegando'
+const msgToUpperCase = winterMessage.toUpperCase()
+console.log(msgToUpperCase)
+
+const indexOfMsg = winterMessage.indexOf('v')
+console.log(indexOfMsg)
+
---
+++ TS/build/01-exercício.js
@@ -0,0 +1,39 @@
+"use strict";
+// EXERCÍCIO 01
+/*
+const myBirthYear: number = 1989;
+const currentYear: number = 2021;
+
+console.log(myBirthYear, currentYear)
+
+const myAge: number = currentYear - myBirthYear;
+
+console.log(myAge)
+
+const ten: number = 10;
+console.log(`
+Em ${currentYear}, estarei com ${myAge + ten} anos`)
+*/
+// EXERCÍCIO 02
+var msg = "Grandes poderes trazem grandes responsabilidades";
+console.log(msg);
+var tom = 'Tom';
+var firstTom = 'Hanks';
+var secondTom = 'Cruise';
+var myFavoriteTom = tom + " " + secondTom;
+console.log(myFavoriteTom);
+var programmingLanguage = 'Javascript';
+var firstJ = programmingLanguage[0];
+var firstS = programmingLanguage[4];
+console.log("\n" + firstJ + firstS);
+var totalLength = programmingLanguage.length;
+console.log(totalLength);
+var randomWord = 'Araponga';
+var lastWord = randomWord[randomWord.length - 1];
+console.log(lastWord);
+var summerMessage = 'O VERÃO CHEGA NESTE FIM DE SEMANA';
+var msgToLowerCase = summerMessage.toLowerCase();
+console.log(msgToLowerCase);
+var winterMessage = 'o inverno está chegando';
+var msgToUpperCase = winterMessage.toUpperCase();
+console.log(msgToUpperCase);
